a7e76119dad9b1c070cd21aae7d77118fafd9ad7
Lauren <llwor94@gmail.com>
2018-09-15 06:57:10 -0700

    ready to deploy

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ server.get('/', (req, res) => {
   res.send('ya made it mon');
 });
 
-const URL = 'https://api.darksky.net/forecast/9a3d59d0aa28de9b80d42982e4546243';
+const wURL =
+  'https://api.darksky.net/forecast/9a3d59d0aa28de9b80d42982e4546243';
 const gURL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=';
 server.post('/location', async (req, res) => {
   let { latitude, longitude } = req.body;
   try {
-    let weather = await axios.get(`${URL}/${latitude},${longitude}`);
+    let weather = await axios.get(`${wURL}/${latitude},${longitude}`);
     let location = await axios.get(
       `${gURL}${latitude},${longitude}&key=AIzaSyCJADyuaUwpH25bQbLBeL5IXR0TrC8xFsA`,
     );
@@ -24,7 +25,7 @@ server.post('/location', async (req, res) => {
     weather = weather.data.currently;
     city = location.data.results[0].address_components[3].long_name;
     state = location.data.results[0].address_components[5].short_name;
-    console.log(weather, city, state);
+    res.json({ weather, city, state });
   } catch (err) {
     console.log(err);
   }
